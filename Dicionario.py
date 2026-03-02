617f00aac15c7815dd35cd77e702a576bd5586b7
Daniel Alvarez <danieleliazalvarezdiaz@gmail.com>
2026-03-02 10:05:35 -0500

    Añadido dicionario y archivo de autenticacion

---
+++ Dicionario.py
@@ -0,0 +1,9 @@
+# se crea el diccionario
+usuarios = { 
+    #cada clave tiene otro dicionario
+    #clave:  Dicionario
+    "1001": {"Numero_Cuenta": "3224716782", "pin": "1234","nombre": "Juan Pérez", "saldo": 1500.0, "movimientos": []},
+    "1002": {"Numero_Cuenta": "3005716782", "pin": "5678","nombre": "María López",  "saldo": 2500.0, "movimientos": []},
+    "1003": {"Numero_Cuenta": "3020715782", "pin": "9012","nombre": "Carlos Gómez", "saldo": 3200.0, "movimientos": []},
+    "1004": {"Numero_Cuenta": "3047197804", "pin": "3456","nombre": "Ana Torres", "saldo": 1000.0, "movimientos": []}
+}
--- clave.py
+++ clave.py
@@ -8,14 +8,6 @@ def cambio_clave(clave_puesta):
     print("clave camviada correctamente")
     return clave_puesta
 
-usuarios = {
-    "1001": {"nombre": "Juan Pérez", "pin": "1234", "saldo": 1500.0},
-    "1002": {"nombre": "María López", "pin": "5678", "saldo": 2500.0},
-    "1003": {"nombre": "Carlos Gómez", "pin": "9012", "saldo": 3200.0},
-    "1004": {"nombre": "Ana Torres", "pin": "3456", "saldo": 1000.0}
-}
-
-
 def obtener_usuario_por_pin(pin):
     for clave, datos in usuarios.items():
         if datos["pin"] == pin:
